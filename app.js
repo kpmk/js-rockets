11ba98fb2bfae0d562766b3b1d7831d5a1f7b33f
kpmk <info@katekeileydesigns.com>
2020-06-12 00:51:50 +1000

    more stuff

---
+++ app.js
@@ -0,0 +1,45 @@
+var timer = null;
+
+var countdownNumber = 10;
+
+var changeState = function (state) {
+    document.body.className = 'body-state'+ state;
+
+    clearInterval(timer);
+    if (state == 2) {
+        timer = setInterval (function (){
+            document.getElementById('countdown').
+        innerHTML = countdownNumber;
+        
+        countdownNumber = countdownNumber -1;
+        var countdownNumber = 10;
+        if (countdownNumber <= 0) {
+            
+            changeState(3);
+
+        };
+
+        }, 500);
+    
+    } else if (state == 3) {
+
+        var sucess = setTimeout (function (){
+
+            var randomNumber = Math.round (Math.random()*10);
+            console.log ('randomNumber;', randomNumber);
+            if (randomNumber > 5){
+
+                changeState (4);
+
+            } else {
+                changeState (5);
+
+            };
+
+        }, 2000);
+    };
+    
+
+    
+    
+};
