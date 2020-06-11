a0d9d3ec50e61883229b2e036f7b92a9a2fe6a64
kpmk <info@katekeileydesigns.com>
2020-06-12 01:18:13 +1000

    almost ready

--- app.js
+++ app.js
@@ -1,45 +1,36 @@
-var timer = null;
-
-var countdownNumber = 10;
-
-var changeState = function (state) {
-    document.body.className = 'body-state'+ state;
-
+var timer=null;
+var countdownNumber=10
+var changeState=function (state) {
+    document.body.className='body-state'+state;
     clearInterval(timer);
-    if (state == 2) {
-        timer = setInterval (function (){
-            document.getElementById('countdown').
-        innerHTML = countdownNumber;
-        
-        countdownNumber = countdownNumber -1;
-        var countdownNumber = 10;
-        if (countdownNumber <= 0) {
-            
-            changeState(3);
+    countdownNumber=10;
+    document.getElementById('countdown').innerHTML=countdownNumber;
 
-        };
+    // countdown
+    if (state==2){
+        timer=setInterval(function () {
+            countdownNumber=countdownNumber-1;
+            document.getElementById('countdown').innerHTML=countdownNumber;
 
-        }, 500);
-    
-    } else if (state == 3) {
-
-        var sucess = setTimeout (function (){
-
-            var randomNumber = Math.round (Math.random()*10);
-            console.log ('randomNumber;', randomNumber);
-            if (randomNumber > 5){
-
-                changeState (4);
-
-            } else {
-                changeState (5);
-
+            if (countdownNumber<=0){
+                changeState(3);
             };
+        },1000);
+    }else if (state==3) {
+        var success=setTimeout(function(){
+                var randomNumber=Math.round(Math.random()*10);
+                
+                console.log('randomNumber'+randomNumber)
 
+                //success
+                if (randomNumber>3) {
+                    changeState(4);
+                    //do something
+                }else{
+                    changeState(5)//oh no!
+                }
         }, 2000);
     };
+}
     
 
-    
-    
-};
